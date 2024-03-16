6a8ff779a1e760edfb0606e979b77dbba254e5ab
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-03-16 23:37:21 +0530

    Basic sertup by create react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// App Component (Kind of HTMl + pure JS)
 
 function App() {
+  const a = 1 + 2;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          utkarsh is nice Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>HEY THERE</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,3 +28,9 @@ function App() {
 }
 
 export default App;
+
+// "react": "^18.2.0", -> MANAGING UI (protecting, opttimzed, secure(innerHtml))
+//     "react-dom": "^18.2.0", -> SHOWING UI
+//     "react-scripts": "5.0.1", -> Interaction b/w REACT and DEVELOPER
+
+// // react people have written Privaacy code, resuability code, Protection code (innerHtml) // manages UI  // shows the UI in desktop // if we want to work with react we need to know how to instruct it.
