a6d09bc5a861934761fdad25db9939eb05ad61a4
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-04-21 23:29:37 +0530

    Assignment: Authentication using react router || PROBLEM STATEMENT

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HelloComponent from "./1.BasicReact/1.HelloComponent/HelloComponent";
 import HelloClassComponent from "./1.BasicReact/1.HelloComponent/HelloClassComponent";
 import RouterApp from "./12.ReactRouting/2.RouterApplicatoin/RouterApp";
 import { BrowserRouter, HashRouter } from "react-router-dom";
+import App from "./00.Assignments/1.Authentication_using_react_router/component/App";
 
 // document keyword is my REAL DOM
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -18,9 +19,13 @@ root.render(
     {/* <App /> */}
     {/* ----------  ROUTER APP ------- */}
 
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <RouterApp />
-    </BrowserRouter>
+    </BrowserRouter> */}
+
+    {/* Assignment */}
+
+    <App />
   </React.StrictMode>
 );
 
