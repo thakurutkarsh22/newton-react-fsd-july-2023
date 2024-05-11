466561dbd0c9278de38f5d944fbff0442e0d2182
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-05-11 23:08:01 +0530

    Handle Expanded and not expanded like folder structure

--- src/0.Interview/2.EmployeeHirarchy/2.Answer/EmployeeHierarchy.js
+++ src/0.Interview/2.EmployeeHirarchy/2.Answer/EmployeeHierarchy.js
@@ -1,20 +1,35 @@
+import { useState } from "react";
+
 function EmployeeHierarchy(props) {
   const { data } = props;
   const { name, hasAnyOneUnderHim, item: subordinates } = data;
   console.log(data, "data");
 
+  const [expanded, setExpanded] = useState(true);
+
   return (
     <>
       {/* WORK FOR PARENT (CEO) */}
 
-      <div>{name}</div>
+      <div
+        onClick={() => {
+          setExpanded((old) => !old);
+        }}
+      >
+        <span>
+          {" "}
+          {expanded ? "v" : ">"} {name}
+        </span>
+      </div>
 
       {/* FAITH IN CHILDREN (CTO, COO, VP etc..... ) */}
-      <div style={{ marginLeft: "15px" }}>
-        {subordinates.map((employee) => {
-          return <EmployeeHierarchy data={employee} />;
-        })}
-      </div>
+      {expanded && (
+        <div style={{ marginLeft: "15px" }}>
+          {subordinates.map((employee) => {
+            return <EmployeeHierarchy data={employee} />;
+          })}
+        </div>
+      )}
     </>
   );
 }
